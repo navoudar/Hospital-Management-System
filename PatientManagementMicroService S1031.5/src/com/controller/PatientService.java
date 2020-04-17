d4911860a9bc7851052dde97a3d4ab5e273de8ad
navoudar <navodudara18@gmail.com>
2020-04-17 23:27:23 +0530

    Initial file upload for Patient management function

---
+++ PatientManagementMicroService S1031.5/src/com/controller/PatientService.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/patient")
+public class PatientService {
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems() {
+
+		return "From  Patient";
+	}
+}
