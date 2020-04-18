94b7bdde6bd6d43306e06e354278d736bc655b36
Vyshna-Sri <vyshnavy30@gmail.com>
2020-04-19 04:57:09 +0530

    Appointment management works with CRUD functions

--- AppointmentManagementMicroService S1031.5/src/com/controller/AppointmentService.java
+++ AppointmentManagementMicroService S1031.5/src/com/controller/AppointmentService.java
@@ -4,11 +4,17 @@ import java.sql.Date;
 import java.sql.Time;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -53,6 +59,81 @@ public class AppointmentService {
 		
 		return output;
 		
+	}
+	
+	@GET
+	@Path("/viewall")
+	@Produces(MediaType.TEXT_HTML)
+	public String viewAllAppointment() {
+		
+		Appointment appointment = new Appointment();
+		
+		return appointment.viewAllAppointment();
+	}
+	
+	/*@POST
+	@Path("/view")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String viewAnAppointment(String details) {
+		
+		String output = "";
+		Appointment appointment = new Appointment();
+		String outPut = "";
+		
+		JsonObject appointmentObj = new JsonParser().parse(details).getAsJsonObject();
+		
+		String m_appointment_patient = appointmentObj.get("m_appointment_patient").getAsString();
+		
+		output = appointment.viewAnAppointment(m_appointment_patient);
+		
+		return output;
+	}*/
+	
+	
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateAppointment(String appointmentData) {
+		
+		
+		Appointment appointment = new Appointment();
+		
+		JsonObject appointmentObj = new JsonParser().parse(appointmentData).getAsJsonObject();
+		
+		String m_appointment_no = appointmentObj.get("m_appointment_no").getAsString();
+		String m_appointment_patient = appointmentObj.get("m_appointment_patient").getAsString();
+		String m_appointment_category = appointmentObj.get("m_appointment_category").getAsString();
+		String m_appointment_hospital = appointmentObj.get("m_appointment_hospital").getAsString();
+		String m_appointment_date = appointmentObj.get("m_appointment_date").getAsString();
+		String m_appointment_time = appointmentObj.get("m_appointment_time").getAsString();
+		String m_appointment_description = appointmentObj.get("m_appointment_description").getAsString();
+				
+				
+		String output = appointment.updateAppointment(m_appointment_no, m_appointment_patient, m_appointment_category, m_appointment_hospital, m_appointment_date, m_appointment_time, m_appointment_description);
+		
+		return output;
+	}
+	
+	
+	
+	@DELETE
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteAppointment(String AppointmentData) {
+
+		Appointment appointmentObj = new Appointment();
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(AppointmentData, "", Parser.xmlParser());
+
+		// Read the value from the element <itemID>
+		String m_appointment_no = doc.select("m_appointment_no").text();
+
+		String output = appointmentObj.deleteAppointment(m_appointment_no);
+
+		return output;
 	}
 	
 	
--- AppointmentManagementMicroService S1031.5/src/com/model/Appointment.java
+++ AppointmentManagementMicroService S1031.5/src/com/model/Appointment.java
@@ -1,10 +1,15 @@
 package com.model;
 
 import java.sql.Connection;
+
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.Time;
+
+
 
 public class Appointment {
 
@@ -69,9 +74,9 @@ public class Appointment {
 			
 			
 
-			output =  "Doctor Details Inserted successfully";
+			output =  "Appointment Details Inserted successfully";
 		} catch (Exception e) {
-			output =  "Error while inserting the Doctor Details .";
+			output =  "Error while inserting the Appointment Details .";
 			System.err.println(e.getMessage());
 		}
 
@@ -79,11 +84,210 @@ public class Appointment {
 	
 	}
 	
+	public String viewAllAppointment() {
+
+		String output = "";
+
+		try
+			{
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for reading.";
+			}
+
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Patient Name</th><th>Specialization Category</th><th>Hospital Name</th><th>Date</th><th>Time</th><th>Appointment Description</th><th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from m_appointment";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String m_appointment_no = Integer.toString(rs.getInt("m_appointment_no"));
+				String m_appointment_patient = rs.getString("m_appointment_patient");
+				String m_appointment_category = rs.getString("m_appointment_category");
+				String m_appointment_hospital = rs.getString("m_appointment_hospital");
+				Date m_appointment_date = rs.getDate("m_appointment_date");
+				Time m_appointment_time = rs.getTime("m_appointment_time");
+				String m_appointment_description = rs.getString("m_appointment_description");
+
+				// Add into the html table
+				output += "<tr><td>" + m_appointment_patient + "</td>";
+				output += "<td>" + m_appointment_category + "</td>";
+				output += "<td>" + m_appointment_hospital + "</td>";
+				output += "<td>" + m_appointment_date + "</td>";
+				output += "<td>" + m_appointment_time + "</td>";
+				output += "<td>" + m_appointment_description + "</td>";
+
+				// buttons
+				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+						+ "<td><form method=\"post\" action=\"Appointment.jsp\">"
+						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+						+ "<input name=\"aNo\" type=\"hidden\" value=\"" + m_appointment_no + "\">" + "</form></td></tr>";
+
+			}
+
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the Appointment.";
+			System.err.println(e.getMessage());
+		}
+
+			
+			return output;
+	}
+	
+		
+	/*public String viewAnAppointment(String m_appointment_patient) {
+		
+		String output = "";
+
+		try
+			{
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for reading.";
+			}
+
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>Patient Name</th><th>Specialization Category</th><th>Hospital Name</th><th>Date</th><th>Time</th><th>Appointment Description</th><th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from m_appointment where m_appointment_patient=?";
+			PreparedStatement preparedstmt = con.prepareStatement(query);
+			ResultSet rs = preparedstmt.executeQuery(query);
+			
+			while (rs.next()) {
+				String m_appointment_no = Integer.toString(rs.getInt("m_appointment_no"));
+				String m_appointment_patient1 = rs.getString("m_appointment_patient");
+				String m_appointment_category = rs.getString("m_appointment_category");
+				String m_appointment_hospital = rs.getString("m_appointment_hospital");
+				Date m_appointment_date = rs.getDate("m_appointment_date");
+				Time m_appointment_time = rs.getTime("m_appointment_time");
+				String m_appointment_description = rs.getString("m_appointment_description");
+
+				// Add into the html table
+				output += "<tr><td>" + m_appointment_patient + "</td>";
+				output += "<td>" + m_appointment_category + "</td>";
+				output += "<td>" + m_appointment_hospital + "</td>";
+				output += "<td>" + m_appointment_date + "</td>";
+				output += "<td>" + m_appointment_time + "</td>";
+				output += "<td>" + m_appointment_description + "</td>";
+
+				// buttons
+				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+						+ "<td><form method=\"post\" action=\"Appointment.jsp\">"
+						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+						+ "<input name=\"aNo\" type=\"hidden\" value=\"" + m_appointment_no + "\">" + "</form></td></tr>";
+
+			}
+
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the Appointment.";
+			System.err.println(e.getMessage());
+		}
+
+			
+			return output;
+		
+	}*/
+	
 		
 		
-		
-		
-		
+	public String updateAppointment(String m_appointment_n0, String m_appointment_patient, String m_appointment_category, String m_appointment_hospital, String m_appointment_date, String m_appointment_time, String m_appointment_description) {
+
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for updating.";
+
+			}
+
+			// create a prepared statement
+			String query = "UPDATE m_appointment SET m_appointment_patient=?,m_appointment_category=?,m_appointment_hospital=?,m_appointment_date=?,m_appointment_time=?,m_appointment_description=?      WHERE m_appointment_no=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setString(1, m_appointment_patient);
+			preparedStmt.setString(2, m_appointment_category);
+			preparedStmt.setString(3, m_appointment_hospital);
+			preparedStmt.setDate(4, Date.valueOf(m_appointment_date));
+			preparedStmt.setTime(5, Time.valueOf(m_appointment_time));
+			preparedStmt.setString(6, m_appointment_description);
+			preparedStmt.setInt(7, Integer.parseInt(m_appointment_n0));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Updated successfully";
+
+		} catch (Exception e) {
+			output = "Error while updating the appointment.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+	
+	
+	public String deleteAppointment(String m_appointment_no) {
+
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for deleting.";
+
+			}
+
+			// create a prepared statement
+
+			String query = "delete from m_appointment where m_appointment_no=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(m_appointment_no));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Deleted successfully";
+
+		} catch (Exception e) {
+			output = "Error while deleting the appointment.";
+			System.err.println(e.getMessage());
+
+		}
+
+		return output;
+
+	}
 		
 		
 		
