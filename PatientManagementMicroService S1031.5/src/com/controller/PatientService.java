80ca0fc7d3509327d0b412038462c949fb210493
navoudar <navodudara18@gmail.com>
2020-04-18 10:13:39 +0530

    Added new files for logging system

--- PatientManagementMicroService S1031.5/src/com/controller/PatientService.java
+++ PatientManagementMicroService S1031.5/src/com/controller/PatientService.java
@@ -1,9 +1,15 @@
 package com.controller;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.model.Patient;
 
 @Path("/patient")
 public class PatientService {
@@ -14,5 +20,20 @@ public class PatientService {
 	public String readItems() {
 
 		return "From  Patient";
+	}
+	
+	@POST
+	@Path("/getDetails")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String getSpecificPatientDetails(String details) {
+		
+		Patient patient = new Patient();
+		
+		JsonObject itemObject = new JsonParser().parse(details).getAsJsonObject();
+		
+		String Uname = itemObject.get("name").getAsString();
+		String output = patient.readItems(Uname);
+		return output;
 	}
 }
--- PatientManagementMicroService S1031.5/src/com/model/Patient.java
+++ PatientManagementMicroService S1031.5/src/com/model/Patient.java
@@ -1,5 +1,93 @@
 package com.model;
 
-public class Patient {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Patient {
+	
+	String name;
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private Connection connect() {
+
+		Connection con = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return con;
+
+	}
+	
+	public String readItems(String Uname) {
+		
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for reading.";
+			}
+
+			// Prepare the html table to be displayed
+			output = "<table border=\"1\"><tr><th>User ID</th><th>User Name</th><th>Name</th><th>Password</th><th>Update</th><th>Remove</th></tr>";
+
+			String query = "select * from m_patient";
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				
+				String UserID = rs.getString("m_patient_id");
+				String UserName = rs.getString("m_patient_userName");
+				String Name = rs.getString("m_patient_name");
+				String Password = rs.getString("m_patient_password");
+
+				// Add into the html table
+				output += "<tr><td>" + UserID + "</td>";
+				output += "<td>" + UserName + "</td>";
+				output += "<td>" + Name + "</td>";
+				output += "<td>" + Password + "</td>";
+
+				// buttons
+				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
+						+ "<td><form method=\"post\" action=\"items.jsp\">"
+						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
+						+ "<input name=\"itemID\" type=\"hidden\" value=\"" + UserID + "\">" + "</form></td></tr>";
+
+			}
+
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the items.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+	
 }
