78cfcf700127d6e7d19e1b0355a5fb7fbd82ad16
IT17165662 <thillainathan1997r@gmail.com>
2020-04-19 14:37:59 +0530

    update Login Function

--- PatientManagementMicroService S1031.5/src/com/controller/PatientService.java
+++ PatientManagementMicroService S1031.5/src/com/controller/PatientService.java
@@ -1,39 +1,152 @@
 package com.controller;
 
+import java.sql.Date;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.protobuf.Parser;
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import com.google.gson.*;
+
+
+
 import com.model.Patient;
 
-@Path("/patient")
+
+
+@Path("/Patient")
 public class PatientService {
+
+	
+	
+	Patient PatientObj = new Patient();
+
 
 	@GET
 	@Path("/")
-	@Produces(MediaType.TEXT_HTML)
-	public String readItems() {
+	@Produces(MediaType.TEXT_XML)
+	public String readPatient() {
 
 		return "From  Patient";
 	}
 	
+	
 	@POST
-	@Path("/getDetails")
+	@Path("/Login")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_HTML)
-	public String getSpecificPatientDetails(String details) {
-		
+	public String PatientLogin(String PatientDetails) {
+		String output="";
 		Patient patient = new Patient();
+		String outPut = "";
 		
-		JsonObject itemObject = new JsonParser().parse(details).getAsJsonObject();
+		JsonObject loginObject = new JsonParser().parse(PatientDetails).getAsJsonObject();
 		
-		String Uname = itemObject.get("name").getAsString();
-		String output = patient.readItems(Uname);
+		
+		String Uname = loginObject.get("userName").getAsString();
+		String password = loginObject.get("password").getAsString();
+		
+		output = patient.PatientLogin(Uname, password);
+		
+return output;		
+		
+	}
+	
+	@POST
+	@Path("/insert")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertPatient(String patientData) {
+
+		// Convert the input string to a JSON object
+		JsonObject patientObject = new JsonParser().parse(patientData).getAsJsonObject();
+		
+		// Read the values from the JSON object
+		String m_patient_id = patientObject.get("m_patient_id").getAsString();
+		String m_patient_userName = patientObject.get("m_patient_userName").getAsString();
+		String m_patient_password = patientObject.get("m_patient_password").getAsString();
+		String m_patient_name = patientObject.get("m_patient_name").getAsString();
+		String m_patient_birthDate = patientObject.get("m_patient_birthDate").getAsString();
+		Double m_patient_weight = patientObject.get("m_patient_weight").getAsDouble();
+		String m_patient_bloodGroup = patientObject.get("m_patient_bloodGroup").getAsString();
+		int m_patient_contactNo = patientObject.get("m_patient_contactNo").getAsInt();
+		String m_patient_address = patientObject.get("m_patient_address").getAsString();
+		String m_patient_gurdianName = patientObject.get("m_patient_gurdianName").getAsString();
+		int m_patient_age = patientObject.get("m_patient_age").getAsInt();
+		
+		String output = PatientObj.insertPatient(m_patient_id,m_patient_userName,m_patient_password,m_patient_name, Date.valueOf(m_patient_birthDate), m_patient_weight, m_patient_bloodGroup, m_patient_contactNo, m_patient_address, m_patient_gurdianName, m_patient_age);
+		
 		return output;
 	}
-}
+	
+	
+	@GET
+	@Path("/view")
+	@Produces(MediaType.TEXT_HTML)
+	public String readPatientDetails() {
+		Patient patient = new Patient();
+		
+		return patient.readPatient();
+	}
+	
+
+	@PUT
+	@Path("/update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updatePatient(String patientData) {
+
+		// Convert the input string to a JSON object
+		JsonObject patientObject = new JsonParser().parse(patientData).getAsJsonObject();
+
+		// Read the values from the JSON object
+		String m_patient_id = patientObject.get("m_patient_id").getAsString();
+		String m_patient_userName = patientObject.get("m_patient_userName").getAsString();
+		String m_patient_password = patientObject.get("m_patient_password").getAsString();
+		String m_patient_name = patientObject.get("m_patient_name").getAsString();
+		String m_patient_birthDate = patientObject.get("m_patient_birthDate").getAsString();
+		Double m_patient_weight = patientObject.get("m_patient_weight").getAsDouble();
+		String m_patient_bloodGroup = patientObject.get("m_patient_bloodGroup").getAsString();
+		int m_patient_contactNo = patientObject.get("m_patient_contactNo").getAsInt();
+		String m_patient_address = patientObject.get("m_patient_address").getAsString();
+		String m_patient_gurdianName = patientObject.get("m_patient_gurdianName").getAsString();
+		int m_patient_age = patientObject.get("m_patient_age").getAsInt();
+		
+		String output = PatientObj.updatePatient(m_patient_id,m_patient_userName,m_patient_password,m_patient_name, Date.valueOf(m_patient_birthDate), m_patient_weight, m_patient_bloodGroup, m_patient_contactNo, m_patient_address, m_patient_gurdianName, m_patient_age);
+
+		return output;
+	}
+
+	
+	@DELETE
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deletePatient(String patientData) {
+		Patient patient = new Patient();
+		JsonObject PatientObj = new JsonParser().parse(patientData).getAsJsonObject();
+		
+		String m_patient_id = PatientObj.get("m_patient_id").getAsString();
+		
+		String output = patient.deletePatient(m_patient_id);
+		return output;
+	}
+	
+
+	
+	
+	
+	}
+
--- PatientManagementMicroService S1031.5/src/com/model/Patient.java
+++ PatientManagementMicroService S1031.5/src/com/model/Patient.java
@@ -1,42 +1,139 @@
 package com.model;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Patient {
 	
-	String name;
+	String userName;
+	String password;
+	String m_patient_id;
+	String m_patient_userName;
+	String m_patient_password ;
+	String m_patient_name;
+	Date m_patient_birthDate;
+	double m_patient_weight;
+	String m_patient_bloodGroup;
+    int m_patient_contactNo;
+    String m_patient_address;
+    String m_patient_gurdianName;
+    int m_patient_age;
+	// A common method to connect to the DB
+		private Connection connect() {
+
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+
+		}
+		
+	
+	
+	public String PatientLogin(String UserName,String Password) {
+		
+		String output = "";
+		
+		try {
+			
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for logging.";
+		}
+		
+			String query = "SELECT m_patient_userName,m_patient_password FROM m_patient WHERE m_patient_userName = ?";
+			
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			preparedStmt.setString(1, UserName);
+			
+			ResultSet rs = preparedStmt.executeQuery();
+			
+				while(rs.next()) {
+					
+					userName = rs.getString("m_patient_userName");
+					password = rs.getString("m_patient_password");
+				}
+			
+			con.close();
+			
+				if(userName.equalsIgnoreCase(UserName) && password.equalsIgnoreCase(Password)) {
+					output = "success";
+				}
+		}catch(Exception e) {
+			
+			output="Failed";
+			System.err.println(e.getMessage());
+		}
+		
+		
+		return output;
+	}
 	
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	private Connection connect() {
-
-		Connection con = null;
+	
+	
+public String insertPatient(String m_patient_id,String m_patient_userName,String m_patient_password,String m_patient_name,Date m_patient_birthDate,Double m_patient_weight,String m_patient_bloodGroup,int m_patient_contactNo,String m_patient_address,String m_patient_gurdianName,int m_patient_age ) {
+		
+		String output = "";
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = connect();
 
-			// Provide the correct details: DBServer/DBName, username, password
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+
+			// create a prepared statement
+			String query = " insert into m_patient(`m_patient_id`,`m_patient_userName`,`m_patient_password`,`m_patient_name`,`m_patient_birthDate`,`m_patient_weight`,`m_patient_bloodGroup`,`m_patient_contactNo`,`m_patient_address`,`m_patient_gurdianName`,`m_patient_age`) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setString(1,m_patient_id );
+			preparedStmt.setString(2,m_patient_userName );
+			preparedStmt.setString(3,m_patient_password );
+			preparedStmt.setString(4, m_patient_name);
+			preparedStmt.setDate(5, m_patient_birthDate);
+			preparedStmt.setDouble(6, m_patient_weight);
+			preparedStmt.setString(7, m_patient_bloodGroup);
+			preparedStmt.setInt(8, m_patient_contactNo);
+			preparedStmt.setString(9, m_patient_address);
+			preparedStmt.setString(10, m_patient_gurdianName);
+			preparedStmt.setInt(11, m_patient_age);
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Patient Inserted successfully";
 		} catch (Exception e) {
-			e.printStackTrace();
+			output = "Error while inserting the Patient.";
+			System.err.println(e.getMessage());
 		}
 
-		return con;
-
+		return output;
+	
 	}
 	
-	public String readItems(String Uname) {
-		
+
+	
+
+	public String readPatient() {
+
 		String output = "";
 
 		try {
@@ -49,31 +146,42 @@ public class Patient {
 			}
 
 			// Prepare the html table to be displayed
-			output = "<table border=\"1\"><tr><th>User ID</th><th>User Name</th><th>Name</th><th>Password</th><th>Update</th><th>Remove</th></tr>";
+			output = "<table border=\"1\"><tr><th>m_patient_id</th><th>m_patient_userName</th><th>m_patient_password</th><th>m_patient_name</th><th>m_patient_birthDate</th><th>m_patient_weight</th><th>m_patient_bloodGroup</th><th>m_patient_contactNo</th><th>m_patient_address</th><th>m_patient_gurdianName</th><th>m_patient_age</th><th>Update</th></tr>";
 
-			String query = "select * from m_patient";
+			String query = "select * from m_Patient";
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 
 			// iterate through the rows in the result set
 			while (rs.next()) {
+				String m_patient_id =rs.getString("m_patient_id");
+				String m_patient_userName =rs.getString("m_patient_userName");
+				String m_patient_password =rs.getString("m_patient_password");
+				String m_patient_name = rs.getString("m_patient_name");
+				Date m_patient_birthDate = rs.getDate("m_patient_birthDate");
+				Double m_patient_weight = rs.getDouble("m_patient_weight");
+				String m_patient_bloodGroup = rs.getString("m_patient_bloodGroup");
+				int m_patient_contactNo = rs.getInt("m_patient_contactNo");
+				String m_patient_address =rs.getString("m_patient_address");
+				String m_patient_gurdianName =rs.getString("m_patient_gurdianName");
+				int m_patient_age = rs.getInt("m_patient_age");
 				
-				String UserID = rs.getString("m_patient_id");
-				String UserName = rs.getString("m_patient_userName");
-				String Name = rs.getString("m_patient_name");
-				String Password = rs.getString("m_patient_password");
-
 				// Add into the html table
-				output += "<tr><td>" + UserID + "</td>";
-				output += "<td>" + UserName + "</td>";
-				output += "<td>" + Name + "</td>";
-				output += "<td>" + Password + "</td>";
+				output += "<tr><td>" + m_patient_id + "</td>";
+				output += "<td>" + m_patient_userName + "</td>";
+				output += "<td>" + m_patient_password + "</td>";
+				output += "<td>" + m_patient_name + "</td>";
+				output += "<td>" + m_patient_birthDate + "</td>";
+				output += "<td>" + m_patient_weight + "</td>";
+				output += "<td>" + m_patient_bloodGroup + "</td>";
+				output += "<td>" + m_patient_contactNo + "</td>";
+				output += "<td>" + m_patient_address + "</td>";
+				output += "<td>" + m_patient_gurdianName + "</td>";
+				output += "<td>" + m_patient_age + "</td>";
 
 				// buttons
 				output += "<td><input name=\"btnUpdate\" type=\"button\" value=\"Update\" class=\"btn btn-secondary\"></td>"
-						+ "<td><form method=\"post\" action=\"items.jsp\">"
-						+ "<input name=\"btnRemove\" type=\"submit\" value=\"Remove\" class=\"btn btn-danger\">"
-						+ "<input name=\"itemID\" type=\"hidden\" value=\"" + UserID + "\">" + "</form></td></tr>";
+						+ "<td><form method=\"post\" action=\"Patient.jsp\">";
 
 			}
 
@@ -82,12 +190,98 @@ public class Patient {
 			// Complete the html table
 			output += "</table>";
 		} catch (Exception e) {
-			output = "Error while reading the items.";
+			output = "Error while reading the Patient.";
 			System.err.println(e.getMessage());
 		}
 
 		return output;
 
 	}
-	
+	public String updatePatient(String m_patient_id,String m_patient_userName,String m_patient_password,String m_patient_name,Date m_patient_birthDate,Double m_patient_weight,String m_patient_bloodGroup,int m_patient_contactNo,String m_patient_address,String m_patient_gurdianName, int m_patient_age ) {
+
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for updating.";
+
+			}
+
+			// create a prepared statement
+			String query = "UPDATE m_patient SET m_patient_userName=?,m_patient_password=?,m_patient_name=?,m_patient_birthDate=?,m_patient_weight=?,m_patient_bloodGroup=?,m_patient_contactNo=?,m_patient_address=?,m_patient_gurdianName=?,m_patient_age=?      WHERE m_patient_id=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			
+			preparedStmt.setString(1,m_patient_userName );
+			preparedStmt.setString(2,m_patient_password );
+			preparedStmt.setString(3, m_patient_name);
+			preparedStmt.setDate(4, m_patient_birthDate);
+			preparedStmt.setDouble(5, m_patient_weight);
+			preparedStmt.setString(6, m_patient_bloodGroup);
+			preparedStmt.setInt(7, m_patient_contactNo);
+			preparedStmt.setString(8, m_patient_address);
+			preparedStmt.setString(9, m_patient_gurdianName);
+			preparedStmt.setInt(10, m_patient_age);
+			preparedStmt.setString(11,m_patient_id );
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Patient Details Updated successfully";
+
+		} catch (Exception e) {
+			output = "Error while updating the item.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+
+	}
+	public String deletePatient(String m_patient_id) {
+
+		String output = "";
+
+		try {
+
+			Connection con = connect();
+
+			if (con == null) {
+
+				return "Error while connecting to the database for deleting.";
+
+			}
+
+			// create a prepared statement
+
+			String query = "delete from m_patient where m_patient_id=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setString(1, m_patient_id);
+			
+			
+			
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			output = "Deleted successfully";
+
+		} catch (Exception e) {
+			output = "Error while deleting the Patient.";
+			System.err.println(e.getMessage());
+
+		}
+
+		return output;
+
+	}
 }
